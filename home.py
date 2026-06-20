5398f3eb08d35ea2aa8bccf73febfc20f4cbc3ae
LuisRaizer <luispedrosrcruz@gmail.com>
2026-06-20 16:57:06 -0300

    atualizando a home

--- home.py
+++ home.py
@@ -6,23 +6,23 @@ from functions.loadsave import salvar, carregar
 
 funcionarios = carregar()
 
-resp = input("Digite o número da opção desejada: \n1 - Adicionar funcionário\n2 - Listar todos os funcionários\n3 - Listar funcionários por cargo\n4 - Listar funcionários ativos/inativos\n5 - Alterar Status de um funcionário\n6 - Editar dados de um funcionário\n")
-
-if resp == "1":
-    funcionarios = adicionar()
-    salvar(funcionarios)
-elif resp == "2":
-    print(listar(funcionarios))
-elif resp == "3":
-    cargo = input("Digite o cargo para listar os funcionários: ")
-    print(listarporcargo(funcionarios, cargo))
-elif resp == "4":
-    print(listarativos(funcionarios))
-elif resp == "5":
-    ativar(funcionarios)
-    salvar(funcionarios)
-elif resp == "6":
-    alterar(funcionarios)
-    salvar(funcionarios)
-else:
-    print("Opção inválida")
+resp = input("Digite o número da opção desejada: \n1 - Adicionar funcionário\n2 - Listar todos os funcionários\n3 - Listar funcionários por cargo\n4 - Listar funcionários ativos/inativos\n5 - Alterar Status de um funcionário\n6 - Editar dados de um funcionário\n7 - Sair\nOpção: ")
+while resp != "7":
+    if resp == "1":
+        funcionarios = adicionar()
+        salvar(funcionarios)
+    elif resp == "2":
+        print(listar(funcionarios))
+    elif resp == "3":
+        cargo = input("Digite o cargo para listar os funcionários: ")
+        print(listarporcargo(funcionarios, cargo))
+    elif resp == "4":
+        print(listarativos(funcionarios))
+    elif resp == "5":
+        ativar(funcionarios)
+    elif resp == "6":
+        alterar(funcionarios)
+        salvar(funcionarios)
+    else:
+        print("Opção inválida")
+    resp = input("Digite o número da opção desejada: \n1 - Adicionar funcionário\n2 - Listar todos os funcionários\n3 - Listar funcionários por cargo\n4 - Listar funcionários ativos/inativos\n5 - Alterar Status de um funcionário\n6 - Editar dados de um funcionário\n7 - Sair\nOpção: ")
